c7ba2604712d38cf961d05f34154464cea28cef3
Vadim Uzhakin <vadimuzhakin@gmail.com>
2021-12-25 15:33:59 +0700

    First commit

---
+++ src/Miles.java
@@ -0,0 +1,13 @@
+public class Miles {
+    public static void main(String[] args) {
+        int ticket = 600;
+        int mile = 20;
+        int bonus = ticket/mile;
+        System.out.println("Ваш бонус: " + bonus + " миля(ей)");
+
+
+
+
+
+    }
+}
